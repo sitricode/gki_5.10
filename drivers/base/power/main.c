055089ef2f4d315d92675d4e4e3382874c73a887
Wein Salema <affanzaidan7@gmail.com>
2025-06-29 08:02:52 +0000

    Revert "cpuidle: don't disable cpuidle when entering suspend"
    This reverts commit 5d63c06b6d12be37ba44fb04735cb9843740fab2.

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -32,6 +32,7 @@
 #include <linux/suspend.h>
 #include <trace/events/power.h>
 #include <linux/cpufreq.h>
+#include <linux/cpuidle.h>
 #include <linux/devfreq.h>
 #include <linux/timer.h>
 #include <linux/wakeup_reason.h>
@@ -748,6 +749,8 @@ void dpm_resume_noirq(pm_message_t state)
 
 	resume_device_irqs();
 	device_wakeup_disarm_wake_irqs();
+
+	cpuidle_resume();
 }
 
 /**
@@ -1345,6 +1348,8 @@ static int dpm_noirq_suspend_devices(pm_message_t state)
 int dpm_suspend_noirq(pm_message_t state)
 {
 	int ret;
+
+	cpuidle_pause();
 
 	device_wakeup_arm_wake_irqs();
 	suspend_device_irqs();
